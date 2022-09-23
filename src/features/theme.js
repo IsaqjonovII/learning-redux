34e23e89ea4b0b2f36608eb3da1847e8a5d5e165
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2022-09-23 18:57:59 +0500

    basics of redux toolkit

---
+++ src/features/theme.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {theme: "dark"};
+
+export const theme = createSlice({
+    name: "theme",
+    initialState,
+    reducers: {
+        changeTheme: (state, action) => {
+            state.theme = action.payload
+        }
+    }
+});
+
+
+export default theme.reducer;
+export const { changeTheme } = theme.actions
